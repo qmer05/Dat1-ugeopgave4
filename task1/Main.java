48989f3b42b3903bb94bb6c0a96a9ead13a64302
Ömer Øcalan <144248086+qmer05@users.noreply.github.com>
2023-09-27 07:36:26 +0200

    Delete task1 directory

--- task1/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-	public static void main(String[] args){
-		Team myTeam = new Team("Dream Team");
-		myTeam.setRank(1);
-		System.out.printf("Team: " + myTeam.getName() + "%nRank: " + myTeam.getRank());
-	}
-}
